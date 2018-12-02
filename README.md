54437368c95671996e0ce7e31954ad5ae61c31cc
Alex Gustafson <alexjgustafson@gmail.com>
2018-12-01 23:25:53 -0500

    exercises 1-4

---
+++ README.md
@@ -0,0 +1,4 @@
+Learn Python the Hard Way
+===
+
+Going through examples and storing them here.
---
+++ ex3.py
@@ -0,0 +1,35 @@
+#Rewrite all numbers as floats
+
+#Print a string
+print("I will now count my chickens:")
+
+#Print a string then a number that's the result of some math
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0 )
+
+#Print a string
+print("Now I will count the eggs:")
+
+#Print a float that's the result of a lot of math
+print( 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+#Print a string 
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#Print a boolean that's the result of a comparison
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+#Print a string then an integer that's the result of simple math
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+#Print a string
+print("Oh, that's why it's False.")
+
+#Print a string
+print("How about some more.")
+
+#Print a string then a boolean that's the result of a comparison
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
