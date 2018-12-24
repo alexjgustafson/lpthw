dd67acfaab83a4aa747ede378ad5e4cd896a6dea
Alex Gustafson <alexjgustafson@gmail.com>
2018-12-23 22:06:14 -0500

    ex11

---
+++ ex11.py
@@ -0,0 +1,13 @@
+# print("How old are you?", end=' ')
+# age = input()
+# print("How tall are you?", end=' ')
+# height = input()
+# print("How much do you weigh?", end=' ')
+# weight = input()
+
+# print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+name = input( 'Name: ' )
+company = input( 'Company: ' )
+job_title = input( 'Job Title: ' )
+print(f"Meet {name}, they work at {company} as a {job_title}")
